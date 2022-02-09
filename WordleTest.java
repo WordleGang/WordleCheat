c7197de724aeac7084e28f9f3030d3794ad491c1
TopherToGo <98913733+TopherToGo@users.noreply.github.com>
2022-02-09 14:38:20 -0600

    Update WordleTest.java

--- WordleTest.java
+++ WordleTest.java
@@ -20,9 +20,9 @@ public class WordleTest {
 			ArrayList<String> wordList = new ArrayList<String>();
 			System.out.println("Pick a word and type it into 'Wordle.'\r\n");
 		
-			//Scanner to see what letters are NOT included in the word
+			//Scanner to see what letters ARE NOT included in the word.
 			Scanner nullCheck = new Scanner(System.in);
-			System.out.println("Accepting letters that aren't in the word of the day... (separate each letter with a space, ex: A E I O U)");
+			System.out.println("Accepting letters that aren't in the word of the day... (separate each letter with a space, ex: a e i o u)");
 			String nullLetters = nullCheck.nextLine().toLowerCase();
 			String[] currNullLetters = nullLetters.split(" ");
 		
@@ -32,7 +32,15 @@ public class WordleTest {
 			}
 		
 			System.out.println("\r\nThe letter(s) " + nullLetters + " have been removed from the word list.\r\n");
-		
+			
+			//Scanner to see what letters ARE included in the word.
+			
+			//Scanner to see if there are any green letters and what positions they are in.
+			Scanner greenCheck = new Scanner(System.in);
+			System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t _ _ l e.)");
+			String greenLetters = greenCheck.nextLine().toLowerCase();
+			String[] currGreenLetters = greenLetters.split(" ");
+			
 			//Reader reads through the entire word list and removes words that have null letters.
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String line = reader.readLine();
