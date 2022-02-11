d3e6732eb111e501b2f9c475b2087984266e8243
TopherToGo <98913733+TopherToGo@users.noreply.github.com>
2022-02-11 14:38:18 -0600

    Update WordleTest.java

--- WordleTest.java
+++ WordleTest.java
@@ -5,13 +5,20 @@ public class WordleTest {
 	public static void main(String[] args) throws Exception{
 	
 	//Creates file to be used later in the reader.
-	File file = new File("C:\\Users\\hsstudent\\Documents\\wordleAnswers.txt");
+	File file = new File("C:\\Users\\hsstudent\\eclipse-workspace\\WordleCheat\\bin\\wordleAnswers.txt");
 	
 	//List of strings to hold all letters that are not in the word.
 	ArrayList<String> totNullLetters = new ArrayList<String>();
 	
+	//Array to hold all green letters in order.
+	String[] totGreenLetters = new String[]{"", "", "", "", ""};
+	
+	
 	//Exit case for main while loop.
 	Boolean wordNotFound = true;
+	
+	//Boolean to determine whether the user has discovered a green letter.
+	Boolean greenFound = false;
 		
 		//Main while loop.
 		while(wordNotFound) 
@@ -26,9 +33,9 @@ public class WordleTest {
 			String nullLetters = nullCheck.nextLine().toLowerCase();
 			String[] currNullLetters = nullLetters.split(" ");
 		
-			for(int i = 0; i < currNullLetters.length; i++)
+			for (int i = 0; i < currNullLetters.length; i++)
 			{
-			totNullLetters.add(currNullLetters[i]);
+				totNullLetters.add(currNullLetters[i]);
 			}
 		
 			System.out.println("\r\nThe letter(s) " + nullLetters + " have been removed from the word list.\r\n");
@@ -41,12 +48,26 @@ public class WordleTest {
 			String greenLetters = greenCheck.nextLine().toLowerCase();
 			String[] currGreenLetters = greenLetters.split(" ");
 			
-			//Reader reads through the entire word list and removes words that have null letters.
+			for (int i = 0; i < currGreenLetters.length; i++)
+			{
+				if (!currGreenLetters[i].equals("_") & !currGreenLetters[i].equals(null) & !currGreenLetters[i].equals("")) 
+				{
+					greenFound = true;
+					totGreenLetters[i] = currGreenLetters[i];
+					
+				}
+			}
+			
+			//Reader reads through the entire word list and removes words that do not fit the criteria.
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String line = reader.readLine();
 			while(line!=null)
 			{
 				Boolean goodWord = true;
+				
+				String[] lineLetters = line.split("");
+				
+				//Null letters.
 				for (int i = 0; i < totNullLetters.size(); i++) 
 				{
 					if (line.contains(totNullLetters.get(i))) 
@@ -54,6 +75,19 @@ public class WordleTest {
 						goodWord = false;
 					}
 				}
+				
+				//Green letters.
+				if (greenFound)
+				{
+					for (int i = 0; i < lineLetters.length; i++)
+					{
+						if (!totGreenLetters[i].equals("") & !lineLetters[i].equals(totGreenLetters[i]))
+						{
+							goodWord = false;
+						}
+					}
+				}
+				
 				if (goodWord) 
 				{
 				wordList.add(line);
