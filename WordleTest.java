dae75b6fd2e38db9d471b8eccbdebbe1cf010db9
TopherToGo <98913733+TopherToGo@users.noreply.github.com>
2022-02-14 13:58:11 -0600

    Update WordleTest.java

--- WordleTest.java
+++ WordleTest.java
@@ -13,25 +13,37 @@ public class WordleTest {
 	//Array to hold all green letters in order.
 	String[] totGreenLetters = new String[]{"", "", "", "", ""};
 	
+	//List of arrays of strings to hold all letters that are in the word but in the wrong place.
+	ArrayList<String[]> totYellowLetters = new ArrayList<String[]>();
 	
 	//Exit case for main while loop.
-	Boolean wordNotFound = true;
+	Boolean wordFound = false;
 	
 	//Boolean to determine whether the user has discovered a green letter.
 	Boolean greenFound = false;
 		
 		//Main while loop.
-		while(wordNotFound) 
+		while(!wordFound) 
 		{
 			//Creates list of words to be temporarily used and changed depending on the data that the user gives the program.
 			ArrayList<String> wordList = new ArrayList<String>();
 			System.out.println("Pick a word and type it into 'Wordle.'\r\n");
+			
+			//Checks to see if user has found word.
+			Scanner foundCheck = new Scanner(System.in);
+			System.out.println("Was the word you typed in correct? (y/n)");
+			String foundResponse = foundCheck.nextLine().toLowerCase();
+			if (foundResponse.equals("y"))
+			{
+				wordFound = true;
+				break;
+			}
 		
 			//Scanner to see what letters ARE NOT included in the word.
 			Scanner nullCheck = new Scanner(System.in);
-			System.out.println("Accepting letters that aren't in the word of the day... (separate each letter with a space, ex: a e i o u)");
+			System.out.println("\r\nAccepting letters that aren't in the word of the day... (ex: aeiou)");
 			String nullLetters = nullCheck.nextLine().toLowerCase();
-			String[] currNullLetters = nullLetters.split(" ");
+			String[] currNullLetters = nullLetters.split("");
 		
 			for (int i = 0; i < currNullLetters.length; i++)
 			{
@@ -40,13 +52,19 @@ public class WordleTest {
 		
 			System.out.println("\r\nThe letter(s) " + nullLetters + " have been removed from the word list.\r\n");
 			
-			//Scanner to see what letters ARE included in the word.
+			//Scanner to see if there are any yellow letters and what positions they are in.
+			Scanner yellowCheck = new Scanner(System.in);
+			System.out.println("Accepting yellow letters in the format that README.txt specifies... (if the word you entered was 'raise' and the yellows were r, s, and e, you would type it as r__se.)");
+			String yellowLetters = yellowCheck.nextLine().toLowerCase();
+			String[] currYellowLetters = yellowLetters.split("");
+			
+			totYellowLetters.add(currYellowLetters);
 			
 			//Scanner to see if there are any green letters and what positions they are in.
 			Scanner greenCheck = new Scanner(System.in);
-			System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t _ _ l e.)");
+			System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t__le.)");
 			String greenLetters = greenCheck.nextLine().toLowerCase();
-			String[] currGreenLetters = greenLetters.split(" ");
+			String[] currGreenLetters = greenLetters.split("");
 			
 			for (int i = 0; i < currGreenLetters.length; i++)
 			{
@@ -54,7 +72,6 @@ public class WordleTest {
 				{
 					greenFound = true;
 					totGreenLetters[i] = currGreenLetters[i];
-					
 				}
 			}
 			
@@ -76,8 +93,24 @@ public class WordleTest {
 					}
 				}
 				
+				//Yellow letters.
+				if (goodWord)
+				{
+					for (int i = 0; i < totYellowLetters.size(); i++)
+					{
+						String[] yellowTest = totYellowLetters.get(i);
+						for (int k = 0; k < yellowTest.length; k++)
+						{
+							if (!yellowTest[k].equals("_") & yellowTest[k].equals(lineLetters[k]))
+							{
+								goodWord = false;
+							}
+						}
+					}
+				}
+				
 				//Green letters.
-				if (greenFound)
+				if (greenFound & goodWord)
 				{
 					for (int i = 0; i < lineLetters.length; i++)
 					{
@@ -88,16 +121,17 @@ public class WordleTest {
 					}
 				}
 				
+				//Adds words that pass all tests to the wordList.
 				if (goodWord) 
 				{
-				wordList.add(line);
+					wordList.add(line);
 				}
 				line = reader.readLine();
 			}
 			reader.close();
 			
 			//Prints out 5 words for the user to choose from for the next iteration.
-			for(int i = 0; i < 5; i++)
+			for(int i = 0; i < wordList.size() && i < 5; i++)
 			{
 				System.out.println(wordList.get(i));
 			}
