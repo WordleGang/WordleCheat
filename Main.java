9289fff41d08a85faeab403afa2e75d984ebcfb8
Rushil_Ram <potatobervagesalad@gmail.com>
2022-02-28 14:21:12 -0600

    Integrated Dictionary Class

--- Main.java
+++ Main.java
@@ -1,28 +1,42 @@
-import java.util.Scanner;
+/**
+ * TODO: Import Chris' dictionary class and Justin's probability class
+ */
+import java.io.File;
+import java.util.*;
 
-public class Main {
-  public static void main(String args[]) {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        File answers = new File("/Users/rush/Desktop/WordleHelper/wordleAnswers.txt");
 
-    // System.out.println("Welcome to the Wordle game enter in the letters");
-    // System.out.print("Input: ");
+        String nullLetters;
+        String yellowLetters;
+        String greenLetters;
+        
+        ArrayList<String> wordList = new ArrayList<String>();
 
-    // Scanner sc = new Scanner(System.in);
-    // String input = sc.nextLine();
+        try (Scanner myScanner = new Scanner(System.in)) 
+        {
+            System.out.print("\r\nAccepting letters that aren't in the word of the day... (ex: aeiou): ");
 
-    // Probability p = new Probability();
+            nullLetters = myScanner.nextLine().toLowerCase();
 
-    String[] arr = { "AAAAA", "EEEEE", "JJJJJ" };
+            System.out.println("Accepting yellow letters in the format that README.txt specifies... (if the word you entered was 'raise' and the yellows were r, s, and e, you would type it as r__se.): ");
 
-    // System.out.println("Input: " + input);
+            yellowLetters = myScanner.nextLine().toLowerCase();
 
-    // System.out.println("dog");
-    Probability prob = new Probability();
-    prob.getProbability("AAAAA");
-    prob.getProbability("EEEEE");
-    prob.getProbability("ROUND");
-    prob.getProbability("YYYYY");
-    prob.getProbability("ZZZZZ");
-    System.out.println(prob.makeList(arr));
+            System.out.print("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t__le.): ");
 
-  }
+            greenLetters = myScanner.nextLine().toLowerCase();
+
+        }
+
+
+        Dictionary cheater = new Dictionary(answers, nullLetters, yellowLetters, greenLetters, wordList);
+
+        cheater.confirmLetters();
+        System.out.println(cheater.makeWordList());
+    }
+
 }
