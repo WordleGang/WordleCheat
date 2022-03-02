9feb7518c2cf561b6740857d1b660a9bb0ca80ed
Justin Garrity <firejustinince2@gmail.com>
2022-03-02 14:36:23 -0600

    Added probability based on letter position

---
+++ PositionProbability.java
@@ -0,0 +1,23 @@
+public class PositionProbability {
+  public final double[] postionZeroProbability = {
+      0.061, 0.075, 0.086, 0.048, 0.031, 0.059, 0.05, 0.03, 0.015, 0.009, 0.009, 0.038, 0.046, 0.016, 0.018, 0.061,
+      0.01, 0.045, 0.158, 0.064, 0.014, 0.018, 0.036, 0.0, 0.003, 0.001
+  };
+
+  public final double[] postionOneProbability = {
+      0.131, 0.007, 0.017, 0.009, 0.105, 0.003, 0.005, 0.062, 0.087, 0.0, 0.004, 0.086, 0.016, 0.038, 0.121, 0.026,
+      0.002, 0.115, 0.007, 0.033, 0.08, 0.006, 0.019, 0.006, 0.01, 0.0
+  };
+
+  public final double[] postionTwoProbability = {
+
+  };
+
+  public final double[] postionThreeProbability = {
+
+  };
+
+  public final double[] positionFourProbability = {
+
+  };
+}
