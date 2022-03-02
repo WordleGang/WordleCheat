57281835f5dd2de2cead55b1abdeaa6f6ef0b4eb
TopherToGo <98913733+TopherToGo@users.noreply.github.com>
2022-03-02 14:32:32 -0600

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,146 +1,83 @@
 import java.io.*;
 import java.util.*;
 
-public class Dictionary{
-	
-	//Wordle answers file to be used in the BufferedReader later on.
-	private File answers;
-
-	//ArrayList of strings to hold all letters that have been found to not be a part of the word.
-	public ArrayList<String> totNullLetters = new ArrayList<String>();
-	
-	public String nullLetters;
-
-	//Array to hold all green letters that have been found in order.
-	public String[] totGreenLetters = new String[]{"", "", "", "", ""};
-	
-	public String greenLetters;
-
-	//ArrayList of arrays of strings to hold all instances of letters that are in the word but in the wrong place.
-	public ArrayList<String[]> totYellowLetters = new ArrayList<String[]>();
-	
-	public String yellowLetters;
-
-	//ArrayList of strings that holds all the words that the target word could be.
-	public ArrayList<String> wordList;
-
-	//Boolean to let the program know whether or not a green letter has been found.
-	public Boolean greenFound = false;
-
-	//Constructor for Dictionary class.
-	public Dictionary(File f, String nl, String yl, String gl, ArrayList<String> wl, Boolean gf){
-		this.answers = f;
-		this.nullLetters = nl;
-		this.yellowLetters = yl;
-		this.greenLetters = gl;
-		this.wordList = wl;
-		this.greenFound = gf;
-  	}
-	
-	//Method to be used by the Main class after receiving input from the user regarding the different types of letters.
-	public void confirmLetters(ArrayList<String> nl, String[] gl, ArrayList<String[]> yl)
-	{
-		totNullLetters = nl;
-		String[] currNullLetters = nullLetters.split("");
-		for (int i = 0; i < currNullLetters.length; i++)
-		{
-			totNullLetters.add(currNullLetters[i]);
-		}
+public class Main {
+	public static void main(String[] args) throws Exception{
 		
-		totYellowLetters = yl;
-		String[] currYellowLetters = yellowLetters.split("");
-		totYellowLetters.add(currYellowLetters);
-
-		totGreenLetters = gl;
-		String[] currGreenLetters = greenLetters.split("");			
-		for (int i = 0; i < currGreenLetters.length; i++)
+		//Creates file object to be used later in the reader.
+		File file = new File("C:\\Users\\hsstudent\\eclipse-workspace\\Wordle Test\\src\\wordleAnswers.txt");
+		
+		//Creates a list of words to be transmitted back and forth between the Dictionary class.
+		ArrayList<String> wordList = new ArrayList<String>();
+		
+		Boolean wordFound = false;
+		
+		Dictionary dict = new Dictionary(null, null, null, null, null, null);
+		
+		ArrayList<String> guessList = new ArrayList<String>();
+	
+		//Main while loop.
+		while(!wordFound) 
 		{
-			if (!currGreenLetters[i].equals("_") & !currGreenLetters[i].equals(null) & !currGreenLetters[i].equals("")) 
-			{
-				greenFound = true;
-				totGreenLetters[i] = currGreenLetters[i];
+				Scanner guessCheck = new Scanner(System.in);
+				
+				System.out.println("Type in the word that you guessed.");
+				guessList.add(guessCheck.nextLine().toLowerCase());
+				
+				//Scanner to see what letters ARE NOT included in the word.
+				System.out.println("Accepting letters that aren't in the word of the day... (ex: aeiou, do NOT type letters that have already appeared as another color)");
+				String nullLetters = guessCheck.nextLine().toLowerCase();
+				if (!nullLetters.equals(""))
+				{
+					System.out.println(" ");
+				}
+			
+				System.out.println("The letter(s) " + nullLetters + " have been removed from the word list.\r\n");
+				
+				//Scanner to see if there are any yellow letters and what positions they are in.
+				System.out.println("Accepting yellow letters in the format that README.txt specifies... (if the word you entered was 'raise' and the yellows were r, s, and e, you would type it as r__se.)");
+				String yellowLetters = guessCheck.nextLine().toLowerCase();
+				if (!yellowLetters.equals(""))
+				{
+					System.out.println(" ");
+				}
+				
+				//Scanner to see if there are any green letters and what positions they are in.
+				System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t__le.)");
+				String greenLetters = guessCheck.nextLine().toLowerCase();
+				if (!greenLetters.equals(""))
+				{
+					System.out.println(" ");
+				}
+				
+				ArrayList<String> runningNull = dict.totNullLetters;
+				String[] runningGreen = dict.totGreenLetters;
+				ArrayList<String[]> runningYellow = dict.totYellowLetters;
+				Boolean greenFound = dict.greenFound;
+
+				dict = new Dictionary(file, nullLetters, yellowLetters, greenLetters, wordList, greenFound);
+				dict.confirmLetters(runningNull, runningGreen, runningYellow);
+				wordList = dict.makeWordList(guessList);
+				
+				System.out.println("-----------\r\n");
+				
+				for(int i = 0; i < wordList.size() && i < 5; i++)
+				{
+					System.out.println(wordList.get(i));
+				}
+				System.out.print("\r\n");
+				
+				System.out.println("Pick a word and type it into 'Wordle.'\r\n");
+				
+				//Checks to see if user has found word.
+				Scanner foundCheck = new Scanner(System.in);
+				System.out.println("Was the word you typed in correct? (y/n)");
+				String foundResponse = foundCheck.nextLine().toLowerCase();
+				if (foundResponse.equals("y"))
+				{
+					wordFound = true;
+				}
+				System.out.println(" ");
 			}
-		}
 	}
-
-	//Method to be used after confirming all information with the user. Goes through the previously established answers File variable and removes words that do not fit the criteria.
-	public ArrayList<String> makeWordList(ArrayList<String> gl)
-	{
-		ArrayList<String> tempWordList = new ArrayList<String>();
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(answers));
-			String line = reader.readLine();
-			while(line!=null)
-			{
-				Boolean goodWord = true;
-				
-				for (String guess : gl)
-				{
-					if (line.equals(guess))
-					{
-						goodWord = false;
-					}
-				}
-				
-				String[] lineLetters = line.split("");
-				
-				//Null letters.
-				for (int i = 0; i < totNullLetters.size(); i++) 
-				{
-					if (line.contains(totNullLetters.get(i))) 
-					{
-						goodWord = false;
-					}
-				}
-				
-				//Yellow letters.
-				if (goodWord)
-				{
-					for (int i = 0; i < totYellowLetters.size(); i++)
-					{
-						String[] yellowTest = totYellowLetters.get(i);
-						for (int k = 0; k < yellowTest.length; k++)
-						{
-							if (!yellowTest[k].equals("_"))
-							{
-								if (line.contains(yellowTest[k]) & !yellowTest[k].equals(lineLetters[k]))
-								{
-									
-								} else
-								{
-									goodWord = false;
-								}
-							}
-						}
-					}
-				}
-				
-				//Green letters.
-				if (greenFound & goodWord)
-				{
-					for (int i = 0; i < lineLetters.length; i++)
-					{
-						if (!totGreenLetters[i].equals("") & !lineLetters[i].equals(totGreenLetters[i]))
-						{
-							goodWord = false;
-						}
-					}
-				}
-				
-				//Adds words that pass all tests to the wordList.
-				if (goodWord) 
-				{
-					tempWordList.add(line);
-				}
-				line = reader.readLine();
-			}
-			reader.close();
-			wordList = tempWordList;
-		}
-		catch (Exception e) 
-		{
-			System.out.print("Exception in Dictionary class: " + e);
-		}
-			return wordList;
-		}
 }
