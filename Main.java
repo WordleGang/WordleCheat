e506eb402fb60d6791586ce41e3518ab12f84b50
TopherToGo <98913733+TopherToGo@users.noreply.github.com>
2022-03-02 13:26:31 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,48 +17,48 @@ public class Main {
 		//Main while loop.
 		while(!wordFound) 
 		{
-				//Scanner to see what letters ARE NOT included in the word.
-				Scanner nullCheck = new Scanner(System.in);
-				System.out.println("Accepting letters that aren't in the word of the day... (ex: aeiou)");
-				String nullLetters = nullCheck.nextLine().toLowerCase();
+			//Scanner to see what letters ARE NOT included in the word.
+			Scanner nullCheck = new Scanner(System.in);
+			System.out.println("Accepting letters that aren't in the word of the day... (ex: aeiou)");
+			String nullLetters = nullCheck.nextLine().toLowerCase();
+		
+			System.out.println("\r\nThe letter(s) " + nullLetters + " have been removed from the word list.\r\n");
 			
-				System.out.println("\r\nThe letter(s) " + nullLetters + " have been removed from the word list.\r\n");
-				
-				//Scanner to see if there are any yellow letters and what positions they are in.
-				Scanner yellowCheck = new Scanner(System.in);
-				System.out.println("Accepting yellow letters in the format that README.txt specifies... (if the word you entered was 'raise' and the yellows were r, s, and e, you would type it as r__se.)");
-				String yellowLetters = yellowCheck.nextLine().toLowerCase();
-				
-				//Scanner to see if there are any green letters and what positions they are in.
-				Scanner greenCheck = new Scanner(System.in);
-				System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t__le.)");
-				String greenLetters = greenCheck.nextLine().toLowerCase();
-				
-				ArrayList<String> runningNull = dict.totNullLetters;
-				String[] runningGreen = dict.totGreenLetters;
-				ArrayList<String[]> runningYellow = dict.totYellowLetters;
-				
-				dict = new Dictionary(file, nullLetters, yellowLetters, greenLetters, wordList);
-				dict.confirmLetters(runningNull, runningGreen, runningYellow);
-				wordList = dict.makeWordList();
-				
-				System.out.print("\r\n");
-				for(int i = 0; i < wordList.size() && i < 5; i++)
-				{
-					System.out.println(wordList.get(i));
-				}
-				System.out.print("\r\n");
-				
-				System.out.println("Pick a word and type it into 'Wordle.'\r\n");
-				
-				//Checks to see if user has found word.
-				Scanner foundCheck = new Scanner(System.in);
-				System.out.println("Was the word you typed in correct? (y/n)");
-				String foundResponse = foundCheck.nextLine().toLowerCase();
-				if (foundResponse.equals("y"))
-				{
-					wordFound = true;
-				}
+			//Scanner to see if there are any yellow letters and what positions they are in.
+			Scanner yellowCheck = new Scanner(System.in);
+			System.out.println("Accepting yellow letters in the format that README.txt specifies... (if the word you entered was 'raise' and the yellows were r, s, and e, you would type it as r__se.)");
+			String yellowLetters = yellowCheck.nextLine().toLowerCase();
+			
+			//Scanner to see if there are any green letters and what positions they are in.
+			Scanner greenCheck = new Scanner(System.in);
+			System.out.println("Accepting green letters in the format that README.txt specifies... (if the word you entered was 'table' and the greens were t, e, and l, you would type it as t__le.)");
+			String greenLetters = greenCheck.nextLine().toLowerCase();
+			
+			ArrayList<String> runningNull = dict.totNullLetters;
+			String[] runningGreen = dict.totGreenLetters;
+			ArrayList<String[]> runningYellow = dict.totYellowLetters;
+			
+			dict = new Dictionary(file, nullLetters, yellowLetters, greenLetters, wordList);
+			dict.confirmLetters(runningNull, runningGreen, runningYellow);
+			wordList = dict.makeWordList();
+			
+			System.out.print("\r\n");
+			for(int i = 0; i < wordList.size() && i < 5; i++)
+			{
+				System.out.println(wordList.get(i));
 			}
+			System.out.print("\r\n");
+			
+			System.out.println("Pick a word and type it into 'Wordle.'\r\n");
+			
+			//Checks to see if user has found word.
+			Scanner foundCheck = new Scanner(System.in);
+			System.out.println("Was the word you typed in correct? (y/n)");
+			String foundResponse = foundCheck.nextLine().toLowerCase();
+			if (foundResponse.equals("y"))
+			{
+				wordFound = true;
+			}
+		}
 	}
 }
